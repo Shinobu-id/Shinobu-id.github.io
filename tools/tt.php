5d701cdab2ff77e282300ff42a7232807a5486ec
Huda <87696166+hudaxcodez@users.noreply.github.com>
2021-08-18 17:20:23 +0800

    Create tt.php

---
+++ tools/tt.php
@@ -0,0 +1,38 @@
+<!-- Source By Isalstore.tech -->
+
+
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Download video tiktok anti watermark">
+    <meta name="author" content="putri">
+    <title>Tiktok Downloader</title>
+    <link rel="icon" href="https://i.ibb.co/0JmCRL1/97fe5e8c-4cae-4de7-ba34-6e8b967441d7.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style type="text/css">
+      @import url('https://fonts.googleapis.com/css2?family=Baloo+Chettan+2:wght@500&display=swap');
+      * {font-family: 'Baloo Chettan 2', Sans-Serif;}body {background: #d3d3d3;}
+    </style>
+  </head>
+<body>
+  <div class="container mb-4">
+    <div class="card mt-4">
+      <div class="card-header">
+        Tiktok Downloader No Watermark
+      </div>
+      <div class="card-body">
+        <form action="" method="post">
+          <div class="form-group">
+            <input type="text" name="url" placeholder="link video tiktok" class="form-control" />
+          </div>
+          <div class="form-group text-center mt-2">
+            <button type="submit" name="submit" class="btn btn-sm btn-warning">download</button>
+          </div>
+        </form>
+      </div>
+    </div>
+      </div>
+</body>
+</html>
